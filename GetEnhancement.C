c8fa73bfcf2201ff95c161d89e466ea50e627de8
Dr JR Brown <jamie.brown@york.ac.uk>
2019-06-13 11:34:23 +0100

    Added damping factor to GetEnhancement

--- GetEnhancement.C
+++ GetEnhancement.C
@@ -1,4 +1,4 @@
-Double_t GetEnhancement(TH1F* hist,Double_t fitMin,Double_t fitMax)
+/*Double_t GetEnhancement(TH1F* hist,Double_t fitMin,Double_t fitMax)
 {
 	TF1* cos2phi=new TF1("cos2phi","[0]*cos(2*x*TMath::Pi()/180)+[1]",-180,180);
 	Double_t amp;
@@ -12,7 +12,7 @@ Double_t GetEnhancement(TH1F* hist,Double_t fitMin,Double_t fitMax)
 	enh=(-amp+off)/(amp+off);
 
 	return enh;
-}
+}*/
 
 Double_t GetEnhancement(TH1F* hist)
 {
@@ -27,26 +27,26 @@ Double_t GetEnhancement(TH1F* hist)
 
 	enh=(-amp+off)/(amp+off);
 
-   Double_t amp_err;
-   Double_t off_err;
-   Double_t enh_err;
+	Double_t amp_err;
+	Double_t off_err;
+	Double_t enh_err;
    
-   Double_t damp;
+	Double_t damp;
    
-   damp=hist->GetXaxis()->GetBinWidth(1);
-   damp=damp*TMath::Pi()/180;
-   damp=TMath::Sin(damp)/damp;
+	damp=hist->GetXaxis()->GetBinWidth(1);
+	damp=damp*TMath::Pi()/180;
+	damp=TMath::Sin(damp)/damp;
 
-   amp_err = cos2phi->GetParError(0);
-   off_err=cos2phi->GetParError(1);
+	amp_err = cos2phi->GetParError(0);
+	off_err=cos2phi->GetParError(1);
  
-   enh_err = 2/pow((off+amp),2)*sqrt(amp*amp*off_err*off_err+off*off*amp_err*amp_err);
+	enh_err = 2/pow((off+amp),2)*sqrt(amp*amp*off_err*off_err+off*off*amp_err*amp_err);
 
-   cout << "Amplitde: " << amp << " +/- " << amp_err << endl
-         << "Offset: " << off << "  +/- " << off_err << endl
-         << "Enhancement: " << enh << " +/- " << enh_err << endl
-         << "Damping factor: " << damp << endl;
-
+	cout << "Amplitde: " << amp << " +/- " << amp_err << endl
+		<< "Offset: " << off << "  +/- " << off_err << endl
+		<< "Enhancement: " << enh << " +/- " << enh_err << endl
+		<< "Damping factor: " << damp << endl
+		<< "Corrected Enhancement: " << enh/damp << " +/- " << enh_err/damp << endl;
 
 	return enh;
 }
