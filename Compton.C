373eb83f482404e86c4702efbe161c0e7a88ad7b
jrbrown81 <jamie.brown@york.ac.uk>
2023-10-20 11:45:09 +0100

    Added runtime options to 'Compton.C', specifically 'quiet' option.

--- Compton.C
+++ Compton.C
@@ -13,7 +13,7 @@ void Compton()
 	Help();
 }
 
-Float_t GetEnergyFromAngle(const Float_t incomingEnergy, const Float_t angle)
+Float_t GetEnergyFromAngle(const Float_t incomingEnergy, const Float_t angle, TString opt="")
 //void GetEnergyFromAngle(const Float_t angle)
 {
 	Float_t eRestMass=511;
@@ -23,21 +23,23 @@ Float_t GetEnergyFromAngle(const Float_t incomingEnergy, const Float_t angle)
 //   Float_t outgoingEnergy = incomingEnergy - incomingEnergy*(1. - TMath::Cos(angle*TMath::DegToRad()))/(2. - TMath::Cos(angle*TMath::DegToRad()));
    Float_t depositedEnergy = incomingEnergy - outgoingEnergy;
    
-   cout << "--------------------------------------" << endl
-        << "<<< Compton Scattering Calculation >>>" << endl
-        << "--------------------------------------" << endl;
-   
-   cout << "Incoming Energy:  " << incomingEnergy << " keV" << endl
-        << "Scattering angle: " << angle << " degrees" << endl
-        << "Outgoing Energy:  " << outgoingEnergy << " keV" << endl
-        << "Energy deposited: " << depositedEnergy << " keV" << endl;
-   
-   cout << "--------------------------------------" << endl;
+   if(!opt.Contains("Q")) {
+     cout << "--------------------------------------" << endl
+          << "<<< Compton Scattering Calculation >>>" << endl
+          << "--------------------------------------" << endl;
+     
+     cout << "Incoming Energy:  " << incomingEnergy << " keV" << endl
+          << "Scattering angle: " << angle << " degrees" << endl
+          << "Outgoing Energy:  " << outgoingEnergy << " keV" << endl
+          << "Energy deposited: " << depositedEnergy << " keV" << endl;
+     
+     cout << "--------------------------------------" << endl;
+   }
    
    return outgoingEnergy;
 }
 
-Float_t GetAngleFromEnergy(const Float_t incomingEnergy, const Float_t depositedEnergy)
+Float_t GetAngleFromEnergy(const Float_t incomingEnergy, const Float_t depositedEnergy, TString opt="")
 //void GetAngleFromEnergy(const Float_t incomingEnergy, const Float_t depositedEnergy)
 //void GetAngleFromEnergy(const Float_t depositedEnergy)
 {
@@ -48,16 +50,18 @@ Float_t GetAngleFromEnergy(const Float_t incomingEnergy, const Float_t deposited
 //   Float_t angle = TMath::ACos(1.-depositedEnergy/(incomingEnergy-depositedEnergy))*TMath::RadToDeg();
    Float_t outgoingEnergy = incomingEnergy - depositedEnergy;
    
-   cout << "--------------------------------------" << endl
-        << "<<< Compton Scattering Calculation >>>" << endl
-        << "--------------------------------------" << endl;
-   
-   cout << "Incoming Energy:  " << incomingEnergy << " keV" << endl
-        << "Scattering angle: " << angle << " degrees" << endl
-        << "Outgoing Energy:  " << outgoingEnergy << " keV" << endl
-        << "Energy deposited: " << depositedEnergy << " keV" << endl;
-   
-   cout << "--------------------------------------" << endl;
+   if(!opt.Contains("Q")) {
+     cout << "--------------------------------------" << endl
+          << "<<< Compton Scattering Calculation >>>" << endl
+          << "--------------------------------------" << endl;
+     
+     cout << "Incoming Energy:  " << incomingEnergy << " keV" << endl
+          << "Scattering angle: " << angle << " degrees" << endl
+          << "Outgoing Energy:  " << outgoingEnergy << " keV" << endl
+          << "Energy deposited: " << depositedEnergy << " keV" << endl;
+     
+     cout << "--------------------------------------" << endl;
+   }
    
    return angle;
 }
