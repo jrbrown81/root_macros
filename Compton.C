a324a3662aac054f309f8315a8a93af82895ab47
jrbrown81 <jamie.brown@york.ac.uk>
2024-11-08 13:50:37 +0000

    Added further error messages to 'Compton.C' if unphysical inputs are given.

--- Compton.C
+++ Compton.C
@@ -16,33 +16,54 @@ void Compton()
 Float_t GetEnergyFromAngle(const Float_t incomingEnergy, const Float_t angle, TString opt="")
 //void GetEnergyFromAngle(const Float_t angle)
 {
+	Bool_t error=0;
+
 	Float_t eRestMass=511;
-   Float_t outgoingEnergy = incomingEnergy/(1.+incomingEnergy/eRestMass*(1.-TMath::Cos(angle*TMath::DegToRad())));
+  Float_t outgoingEnergy = incomingEnergy/(1.+incomingEnergy/eRestMass*(1.-TMath::Cos(angle*TMath::DegToRad())));
 
 //   Float_t incomingEnergy=511;
 //   Float_t outgoingEnergy = incomingEnergy - incomingEnergy*(1. - TMath::Cos(angle*TMath::DegToRad()))/(2. - TMath::Cos(angle*TMath::DegToRad()));
-   Float_t depositedEnergy = incomingEnergy - outgoingEnergy;
+  Float_t depositedEnergy = incomingEnergy - outgoingEnergy;
 
-   if(!opt.Contains("Q")) {
-     cout << "--------------------------------------" << endl
-          << "<<< Compton Scattering Calculation >>>" << endl
-          << "--------------------------------------" << endl;
+	if(incomingEnergy<=0) {
+		error=1;
+		if(!opt.Contains("Q")) {
+			cout << "!!!!!!!!!!!! Error !!!!!!!!!!!!" << endl
+				  	<< "Incoming Energy must be > 0." << endl
+				 	  << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl;
+		}
+	} else if(angle<0 || angle >180) {
+		error=1;
+		if(!opt.Contains("Q")) {
+			cout << "!!!!!!!!!!!! Error !!!!!!!!!!!!" << endl
+					 << "Angle must be between 0 and 180 degrees." << endl
+				 	 << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl;
+		}
+	} else {
+		if(!opt.Contains("Q")) {
+  		cout << "--------------------------------------" << endl
+       	 	<< "<<< Compton Scattering Calculation >>>" << endl
+       	 	<< "--------------------------------------" << endl;
 
-     cout << "Incoming Energy:  " << incomingEnergy << " keV" << endl
-          << "Scattering angle: " << angle << " degrees" << endl
-          << "Outgoing Energy:  " << outgoingEnergy << " keV" << endl
-          << "Energy deposited: " << depositedEnergy << " keV" << endl;
+  			cout << "Incoming Energy:  " << incomingEnergy << " keV" << endl
+         	<< "Scattering angle: " << angle << " degrees" << endl
+       	 	<< "Outgoing Energy:  " << outgoingEnergy << " keV" << endl
+       	 	<< "Energy deposited: " << depositedEnergy << " keV" << endl;
 
-     cout << "--------------------------------------" << endl;
-   }
+    	cout << "--------------------------------------" << endl;
+  	}
+	}
 
-   return outgoingEnergy;
+  if(error) return 0;
+	else return outgoingEnergy;
 }
 
 Float_t GetAngleFromEnergy(const Float_t incomingEnergy, const Float_t depositedEnergy, TString opt="")
 //void GetAngleFromEnergy(const Float_t incomingEnergy, const Float_t depositedEnergy)
 //void GetAngleFromEnergy(const Float_t depositedEnergy)
 {
+	Bool_t error=0;
+
   Float_t eRestMass=511;
 	Float_t arg=1.+eRestMass/incomingEnergy-eRestMass/(incomingEnergy-depositedEnergy);
 	Float_t angle = TMath::ACos(arg)*TMath::RadToDeg();
@@ -52,30 +73,50 @@ Float_t GetAngleFromEnergy(const Float_t incomingEnergy, const Float_t deposited
 //   Float_t angle = TMath::ACos(1.-depositedEnergy/(incomingEnergy-depositedEnergy))*TMath::RadToDeg();
   Float_t outgoingEnergy = incomingEnergy - depositedEnergy;
 
-   if(!opt.Contains("Q")) {
-     cout << "--------------------------------------" << endl
-          << "<<< Compton Scattering Calculation >>>" << endl
-          << "--------------------------------------" << endl;
-
+	if(incomingEnergy<=0) {
+		error=1;
+		if(!opt.Contains("Q")) {
+			cout << "!!!!!!!!!!!! Error !!!!!!!!!!!!" << endl
+				  	<< "Incoming Energy must be > 0." << endl
+				 	  << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl;
+		}
+	} else if(depositedEnergy<0) {
+			error=1;
+			if(!opt.Contains("Q")) {
+				cout << "!!!!!!!!!!!! Error !!!!!!!!!!!!" << endl
+				  	 << "Deposited Energy must be >= 0." << endl
+				 	   << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl;
+			}
+	} else {
 		if(arg<-1) {
-			cout << "\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl
-					 << "!!!!!!!!!!!!!! WARNING !!!!!!!!!!!!!!!" << endl
-					 << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl << endl
-					 << "For an incoming energy of " << incomingEnergy << " keV, the" << endl
-					 << "maximum energy deposit is " << incomingEnergy-GetEnergyFromAngle(incomingEnergy,180,"Q") << " keV." << endl
-					 << "The following output will be incorrect!"	<< endl
-           << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl << endl;
+			error=1;
+			if(!opt.Contains("Q")) {
+				cout << "\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl
+					 	 << "!!!!!!!!!!!!!!! WARNING !!!!!!!!!!!!!!!" << endl
+					 	 << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl
+					 	 << "For an incoming energy of " << incomingEnergy << " keV, the" << endl
+					 	 << "maximum energy deposit is " << incomingEnergy-GetEnergyFromAngle(incomingEnergy,180,"Q") << " keV." << endl
+					 	 << "The following output will be incorrect!"	<< endl
+           	 << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl << endl;
+			}
 		}
 
-     cout << "Incoming Energy:  " << incomingEnergy << " keV" << endl
-          << "Scattering angle: " << angle << " degrees" << endl
-          << "Outgoing Energy:  " << outgoingEnergy << " keV" << endl
-          << "Energy deposited: " << depositedEnergy << " keV" << endl;
+		if(!opt.Contains("Q")) {
+    	cout << "--------------------------------------" << endl
+         	<< "<<< Compton Scattering Calculation >>>" << endl
+         	<< "--------------------------------------" << endl;
 
-     cout << "--------------------------------------" << endl;
-   }
+    	cout << "Incoming Energy:  " << incomingEnergy << " keV" << endl
+         	<< "Scattering angle: " << angle << " degrees" << endl
+         	<< "Outgoing Energy:  " << outgoingEnergy << " keV" << endl
+         	<< "Energy deposited: " << depositedEnergy << " keV" << endl;
 
-   return angle;
+    	cout << "--------------------------------------" << endl;
+  	}
+	}
+
+  if(error) return 0;
+	else return angle;
 }
 
 void Help()
