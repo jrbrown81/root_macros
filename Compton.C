e96a5e2fb415eb497f3fdc8cf98a3a83d64c51f3
jrbrown81 <jamie.brown@york.ac.uk>
2024-11-08 13:14:37 +0000

    'Compton.C' now issues a warning if unphysical energy deposits are given as an input.

--- Compton.C
+++ Compton.C
@@ -22,20 +22,20 @@ Float_t GetEnergyFromAngle(const Float_t incomingEnergy, const Float_t angle, TS
 //   Float_t incomingEnergy=511;
 //   Float_t outgoingEnergy = incomingEnergy - incomingEnergy*(1. - TMath::Cos(angle*TMath::DegToRad()))/(2. - TMath::Cos(angle*TMath::DegToRad()));
    Float_t depositedEnergy = incomingEnergy - outgoingEnergy;
-   
+
    if(!opt.Contains("Q")) {
      cout << "--------------------------------------" << endl
           << "<<< Compton Scattering Calculation >>>" << endl
           << "--------------------------------------" << endl;
-     
+
      cout << "Incoming Energy:  " << incomingEnergy << " keV" << endl
           << "Scattering angle: " << angle << " degrees" << endl
           << "Outgoing Energy:  " << outgoingEnergy << " keV" << endl
           << "Energy deposited: " << depositedEnergy << " keV" << endl;
-     
+
      cout << "--------------------------------------" << endl;
    }
-   
+
    return outgoingEnergy;
 }
 
@@ -43,33 +43,45 @@ Float_t GetAngleFromEnergy(const Float_t incomingEnergy, const Float_t deposited
 //void GetAngleFromEnergy(const Float_t incomingEnergy, const Float_t depositedEnergy)
 //void GetAngleFromEnergy(const Float_t depositedEnergy)
 {
-   Float_t eRestMass=511;
-	Float_t angle = TMath::ACos(1.+eRestMass/incomingEnergy-eRestMass/(incomingEnergy-depositedEnergy))*TMath::RadToDeg();
-   
+  Float_t eRestMass=511;
+	Float_t arg=1.+eRestMass/incomingEnergy-eRestMass/(incomingEnergy-depositedEnergy);
+	Float_t angle = TMath::ACos(arg)*TMath::RadToDeg();
+	// Float_t angle = TMath::ACos(1.+eRestMass/incomingEnergy-eRestMass/(incomingEnergy-depositedEnergy))*TMath::RadToDeg();
+
 //   Float_t incomingEnergy=511;
 //   Float_t angle = TMath::ACos(1.-depositedEnergy/(incomingEnergy-depositedEnergy))*TMath::RadToDeg();
-   Float_t outgoingEnergy = incomingEnergy - depositedEnergy;
-   
+  Float_t outgoingEnergy = incomingEnergy - depositedEnergy;
+
    if(!opt.Contains("Q")) {
      cout << "--------------------------------------" << endl
           << "<<< Compton Scattering Calculation >>>" << endl
           << "--------------------------------------" << endl;
-     
+
+		if(arg<-1) {
+			cout << "\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl
+					 << "!!!!!!!!!!!!!! WARNING !!!!!!!!!!!!!!!" << endl
+					 << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl << endl
+					 << "For an incoming energy of " << incomingEnergy << " keV, the" << endl
+					 << "maximum energy deposit is " << incomingEnergy-GetEnergyFromAngle(incomingEnergy,180,"Q") << " keV." << endl
+					 << "The following output will be incorrect!"	<< endl
+           << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl << endl;
+		}
+
      cout << "Incoming Energy:  " << incomingEnergy << " keV" << endl
           << "Scattering angle: " << angle << " degrees" << endl
           << "Outgoing Energy:  " << outgoingEnergy << " keV" << endl
           << "Energy deposited: " << depositedEnergy << " keV" << endl;
-     
+
      cout << "--------------------------------------" << endl;
    }
-   
+
    return angle;
 }
 
 void Help()
 {
-   cout << "GetAngleFromEnergy(const Float_t incomingEnergy, const Float_t depositedEnergy), or" << endl
-	<< "GetEnergyFromAngle(const Float_t incomingEnergy, const Float_t angle)" << endl;
+   cout << "GetAngleFromEnergy(const Float_t incomingEnergy, const Float_t depositedEnergy, TString opt=\"\"), or" << endl
+	<< "GetEnergyFromAngle(const Float_t incomingEnergy, const Float_t angle, TString opt=\"\")" << endl;
 //   cout << "GetAngleFromEnergy(const Float_t depositedEnergy), or" << endl
 //	<< "GetEnergyFromAngle(const Float_t angle)" << endl;
 }
