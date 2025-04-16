0aa3e63089a7732f1520c707211dfc8f3ab4d51e
jrbrown81 <jamie.brown@york.ac.uk>
2025-04-16 17:37:26 +0100

    Added a copy of '.rootlogon.C' to here so it's easier to share.

---
+++ .rootlogon.C
@@ -0,0 +1,9 @@
+{
+// To prevent this from executing when ROOT is started do:$ root -n
+// Will be superceded by a local .rootlogon.C (I think)
+// J. Brown, 16/04/25
+
+	cout << "~/.rootlogon.C executed." << endl;
+	if(!gROOT->ProcessLine(".L myUsefulMacros.C")) cout << "myUsefulMacros.C has been loaded." << endl;
+
+}	
